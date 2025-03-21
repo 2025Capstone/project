ebf9a55f14aa5a00b6241462eb267cbf2696cc86
Hyunuk Shin <shu914@naver.com>
2025-03-21 18:45:18 +0900

    feat : 웹소켓을 통한 랜드마크 전달 기능 개발

--- be/app/main.py
+++ be/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.api.routes import student
 from app.api.routes import video
-
+from app.api.routes import websocket
 app = FastAPI(title="FastAPI Example")
 
 # 라우트 등록
 app.include_router(student.router, prefix="/students", tags=["students"])
 app.include_router(video.router, prefix="/videos", tags=["videos"])
+
+app.include_router(websocket.router, prefix="/ws", tags=["websocket"])
