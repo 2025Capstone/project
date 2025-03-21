d048ed7adfcd13e118b217a8a13680577c586b39
Hyunuk Shin <shu914@naver.com>
2025-03-21 11:55:40 +0900

    feat : HLS 재생 코드 추가

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -2,15 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Hls from "react-hls-player";
+
 
 import MediaPipeFaceMesh from './MediaPipeFaceMesh';
 
 function App() {
   return (
     <div>
-      <h1>MediaPipe Face Mesh</h1>
       <MediaPipeFaceMesh />
-    </div>
+        <Hls
+            src="https://shubusket.s3.ap-northeast-2.amazonaws.com/hls/5cd3c1e9-4544-4e33-af7e-2827553da39e/playlist.m3u8"
+            autoPlay={false}
+            controls={true}
+            width="100%"
+            height="auto"
+        />    </div>
   );
 }
 
