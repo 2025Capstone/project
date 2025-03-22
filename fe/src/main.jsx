fd2d4b850b788542b53e02312f9acfd977056f83
Hyunuk Shin <shu914@naver.com>
2025-03-22 18:45:47 +0900

    feat : mediapipe의 측정 시간은 30fps로 제한

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+    // 개발 이후에 StrictMode를 제거하고 배포
+    <StrictMode>
     <App />
   </StrictMode>,
 )
